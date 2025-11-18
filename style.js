641ad97e4c27101c22a6da9bb475ea0aecb5c917
Mithlesh <aryanrajnwd9155@gmail.com>
2025-11-18 18:25:14 +0530

    second dom

--- style.js
+++ style.js
@@ -1,17 +1,23 @@
-let h2 = document.querySelector('h2');
-let ince = document.getElementById('ince');
+let main = document.querySelector('main');
+let button = document.querySelector('button');
 
- count = 0;
+button.addEventListener('click', function() {
+    let div=document.createElement('div');
 
-ince.addEventListener('click', function() {
-    count++;
-    h2.innerHTML = count;
+    let a= Math.random()*100;
+    let b= Math.random()*100;
+    let c1 = Math.floor(Math.random()*255);
+    let c2 = Math.floor(Math.random()*255);
+    let c3 = Math.floor(Math.random()*255);
+    
 
-});
+    div.style.width='100px';
+    div.style.height='100px';
+    div.style.backgroundColor='rgb(' + c1 + ',' + c2 + ',' + c3 + ')';
+    div.style.position='absolute'; 
+    main.appendChild(div);
+    div.style.left= a + '%';
+    div.style.top= b + '%';
+    div.style.rotate= Math.random()*360 + 'deg';
 
-let dec = document.getElementById('dec');
-
-dec.addEventListener('click', function() {
-    count--;
-    h2.innerHTML = count;
 });
