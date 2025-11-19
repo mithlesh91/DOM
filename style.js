9bc9b2f2036d1a451e75839993e91dbd546a1bb8
Mithlesh <aryanrajnwd9155@gmail.com>
2025-11-19 17:44:16 +0530

    settimeout setinerval

--- style.js
+++ style.js
@@ -1,23 +1,25 @@
-let main = document.querySelector('main');
-let button = document.querySelector('button');
+let btn = document.querySelector("button");
+let h2 = document.querySelector("h2");
+let inner = document.querySelector("#inner");
 
-button.addEventListener('click', function() {
-    let div=document.createElement('div');
 
-    let a= Math.random()*100;
-    let b= Math.random()*100;
-    let c1 = Math.floor(Math.random()*255);
-    let c2 = Math.floor(Math.random()*255);
-    let c3 = Math.floor(Math.random()*255);
-    
 
-    div.style.width='100px';
-    div.style.height='100px';
-    div.style.backgroundColor='rgb(' + c1 + ',' + c2 + ',' + c3 + ')';
-    div.style.position='absolute'; 
-    main.appendChild(div);
-    div.style.left= a + '%';
-    div.style.top= b + '%';
-    div.style.rotate= Math.random()*360 + 'deg';
+let a = 0;
 
-});
+btn.addEventListener("click", function() {
+
+     btn.style.pointerEvents = "none";
+
+    let clear = setInterval(() => {
+        a++;
+        inner.style.width = a + "%";
+        h2.innerHTML = a + "%";
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(clear);
+        h2.innerHTML = "Download Complete";
+        btn.style.opacity = "0.5";
+    }, 10000 );
+
+})
