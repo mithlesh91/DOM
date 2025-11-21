1071e9b9714a2a72ade2cee14162469251b5d45c
Mithlesh <aryanrajnwd9155@gmail.com>
2025-11-21 22:22:57 +0530

    arry data

--- style.js
+++ style.js
@@ -1,16 +1,34 @@
-let heart = document.querySelector('#heart');
-let img = document.querySelector('img');
+const users = [
+  {
+    username: "john_doe",
+    age: 25,
+    profession: "Engineer"
+  },
+  {
+    username: "alice123",
+    age: 30,
+    profession: "Designer"
+  },
+  {
+    username: "mike_k",
+    age: 28,
+    profession: "Teacher"
+  }
+];
+
+console.log(users);
 
 
+let sum = "";
 
-img.addEventListener('dblclick', function() {
-    heart.style.opacity = '1';
-    heart.style.transform = 'translate(-50%, -50%) scale(1)';
+users.forEach(function(users){   
+    sum = sum + `   <div class="card">
+            <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="profile picture">
+            <h2>${users.username}</h2>
+            <h3>${users.age}</h3>
+            <p>${users.profession}</p>
+        </div>`
+})
 
-    console.log('double clicked');
-
-    setTimeout(function() {
-        heart.style.opacity = '0';
-        heart.style.transform = 'translate(-50%, -50%) scale(0)';
-    }, 1000);
-});
+let main = document.querySelector("main");
+main.innerHTML = sum;
